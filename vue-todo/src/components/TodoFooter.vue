72a88e10c3ec01c6d8351a0c8c8ebf5ac79e4e32
RDDcat <yukmaro@gmail.com>
2023-12-19 20:29:04 +0900

    [ADD] 프로젝트 생성, 컴포넌트 4가지 작성 및 링크 연결, 기본 화면 디자인 추가, 폰트 추가

---
+++ vue-todo/src/components/TodoFooter.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="input">
+        footer
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ vue-todo/src/components/TodoInput.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="input">
+        <input type="text" v-model="newTodoItem">
+    </div>
+</template>
+
+<script>
+export default {
+    data(){
+        return {
+            newTodoItem: "",
+        }
+    }
+
+}
+</script>
+<style scoped>
+
+</style>
