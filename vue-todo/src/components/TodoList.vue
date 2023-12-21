6583a4ce7f1bfa37c5d0acaafda7b2ddda85d55b
RDDcat <yukmaro@gmail.com>
2023-12-21 23:51:11 +0900

    [UPDATE] todoList 데이터 넣기

--- vue-todo/src/components/TodoList.vue
+++ vue-todo/src/components/TodoList.vue
@@ -1,12 +1,30 @@
 <template>
     <div class="input">
-        list
+        <ul>
+            <li v-for="todoItem in todoItems" :key="todoItem">{{todoItem}}</li>
+        </ul>
     </div>
 </template>
 
 <script>
 export default {
+    data(){
+        return{
+            todoItems:[],
+        }
+    },
 
+    created(){
+        console.log("created");
+        // 로컬에 데이터가 있다면
+        if(localStorage.length >0){
+            for(var i=0; i< localStorage.length; i++){
+                if(localStorage.key(i)!== 'loglevel:webpack-dev-server'){
+                    this.todoItems.push(localStorage.key(i));
+                }
+            }
+        }
+    }
 }
 </script>
 
