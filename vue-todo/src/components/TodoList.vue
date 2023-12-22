314b7bff321c402a264cd84e29849ca3fa189926
RDDcat <yukmaro@gmail.com>
2023-12-22 18:44:27 +0900

    [UPDATE] todoList 삭제하기 기능추가

--- vue-todo/src/components/TodoList.vue
+++ vue-todo/src/components/TodoList.vue
@@ -1,7 +1,12 @@
 <template>
     <div class="input">
         <ul>
-            <li v-for="todoItem in todoItems" :key="todoItem">{{todoItem}}</li>
+            <li v-for="(todoItem, index) in todoItems" :key="todoItem" class="shadow">
+                {{todoItem}}
+                <span class="removeBtn" @click="removeTodo(todoItem, index)">
+                    <i class="fa-solid fa-trash"></i>
+                </span>
+            </li>
         </ul>
     </div>
 </template>
@@ -11,6 +16,13 @@ export default {
     data(){
         return{
             todoItems:[],
+        }
+    },
+    methods:{
+        removeTodo: function(todoItem, index){
+            console.log(todoItem, index);
+            localStorage.removeItem(todoItem);
+            this.todoItems.splice(index, 1);
         }
     },
 
@@ -29,5 +41,36 @@ export default {
 </script>
 
 <style scoped>
-
+ul{
+    list-style-type: none;
+    padding-left:0px;
+    margin-top: 0;
+    text-align: left;
+}
+li{
+    display: flex;
+    min-height: 50px;
+    height: 50px;
+    line-height: 50px;
+    margin: 0.5rem 0;
+    padding: 0 0.9rem;
+    background: white;
+    border-radius: 5px
+}
+.checkBtn{
+    line-height: 45px;
+    color:#62acde;
+    margin-right: 5px;
+}
+.chechBtnCompleted{
+    color: #b3adad;
+}
+.textCompleted{
+    text-decoration: line-through;
+    color: #b3adad;
+}
+.removeBtn{
+    margin-left: auto;
+    color: #de4343;
+}
 </style>
