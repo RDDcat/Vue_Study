d59667ca75cdf7914e0ac887fe8de1d24f04f50b
RDDcat <yukmaro@gmail.com>
2023-12-30 01:09:04 +0900

    [UPDATE] 데이터 clear 기능 추가

--- vue-todo/src/components/TodoFooter.vue
+++ vue-todo/src/components/TodoFooter.vue
@@ -1,15 +1,33 @@
 <template>
-    <div class="input">
-        footer
+    <div class="clearAllContainer">
+        <span class="clearAllBtn" @click="clearTodo">
+            Clear All
+        </span>
     </div>
 </template>
 
 <script>
 export default {
+    methods:{
+        clearTodo(){
+            localStorage.clear();
+        }
+    }
 
 }
 </script>
 
 <style scoped>
-
+.clearAllContainer{
+    width: 8.5rem;
+    height: 50px;
+    line-height: 50px;
+    background-color: #ffffff;
+    border-radius: 5px;
+    margin: 0 auto;
+}
+.clearAllBtn{
+    color: #e20303;
+    display: block;
+}
 </style>
